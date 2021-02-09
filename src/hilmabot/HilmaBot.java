e7c0f246573f53b275732eb84a0ae1b7654d155a
lov3b <lovebillenius@protonmail.com>
2021-02-09 21:44:36 +0100

    Final commit

--- src/hilmabot/HilmaBot.java
+++ src/hilmabot/HilmaBot.java
@@ -19,18 +19,23 @@ public class HilmaBot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {   
+        // Langa in Token istället för args[0] eller använd kommandolinjen
+        if (args.length < 1) {
+            System.out.println("Du måste langa in en token");
+            System.exit(1);
+        }
         JDABuilder builder = JDABuilder.createDefault(args[0]);
         
         builder.setBulkDeleteSplittingEnabled(false);
-        builder.setActivity(Activity.watching("Hilma ta sin jävla medicin"));
+        builder.setActivity(Activity.watching("Din mamma"));
         
-        try {
+        try{
             builder.build();
         }catch(Exception e) {
-            System.out.print("Något gick helt jävla fel (e.getMessage())");
+            System.out.print("Något gick helt jävla fel: " + e.getMessage());
         }
-        
-        
     }
+     
+
     
-}
+    }
